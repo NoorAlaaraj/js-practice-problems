49da9723a5c5d9241a870b61e8abdcc0a355a709
NoorAlaaraj <Noor_alaaraj@outlook.com>
2022-09-22 09:00:48 +0300

    Submitted

--- practice.js
+++ practice.js
@@ -3,20 +3,48 @@ console.log("practice");
 // Write a function called 'average' that accepts a list of
 // numbers and returns the average. The average
 // of an empty list should be considered zero.
+
 function average() {
-  return undefined;
-}
-console.log(average([1, 2, 3]))
-console.log(average([]))
+  let average = (arr) => {
+    if (arr.length === 0) {
+      return undefined;
+    }
+    let sum = 0
+    for (let i = 0; arr.length; i++) {
+      sum += arr[i]
+    }
+    avg = sum / arr.length
+    return avg
+  }
+  console.log(average([1, 2, 3]))
+  console.log(average([]))
 
 // Write a function called `biggest` that returns the
 // biggest number in an array. If there is no biggest
 // number the function should return undefined.
 function biggest() {
   return undefined;
-}
-console.log(biggest([12,3,4,567,8]))
-console.log(biggest([]))
+
+
+  function biggest(arr) {
+
+    if (arr.length === 0) {
+      return undefined;
+    } else {
+
+      let biggest = arr[0];
+      for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > biggest) {
+          biggest = arr[i];
+        }
+      }
+      return biggest;
+    }
+
+  }
+  console.log(biggest([12, 3, 4, 567, 8]))
+  console.log(biggest([]))
+
 
 // Write a function called `urlParams` that accepts a
 // string representing a URL and returns an object with
@@ -40,7 +68,19 @@ console.log(biggest([]))
 
 function urlParams(query) {
   return undefined;
+  let url = query.split('?');
+  if(url.length ===1){
+    return {}
+  }
+  let params = url[1].split('&')
+  let obj = {}
+  for(let i = 0; i<params.length; i++){
+    let keyVal = params[i].split('=')
+    obj[keyVal[0]]=keyVal[1]
+  }
+  return obj;
 }
+
 console.log(urlParams("http://google.com/"))
 console.log(urlParams("http://google.com/?q=cats"))
 console.log(urlParams("http://google.com/?q=cats&type=images"))
